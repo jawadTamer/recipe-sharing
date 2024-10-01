3fcc0ffaee7a0aeed45437da48926adcf7b09d3e
Saja Malek <66196100+sajamalek90@users.noreply.github.com>
2024-10-01 05:29:26 +0300

    Update signup.component.ts

--- src/app/4.signup/signup.component.ts
+++ src/app/4.signup/signup.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, NgClass],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
 
+export class SignupComponent {
+  isSignUpMode: boolean = false;
+
+  toggleSignUp() {
+    this.isSignUpMode = true;
+  }
+
+  toggleSignIn() {
+    this.isSignUpMode = false;
+  }
 }
