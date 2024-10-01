d958106c1df86ea61d6af0aca4b875c12a850187
Saja Malek <66196100+sajamalek90@users.noreply.github.com>
2024-10-01 05:21:31 +0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { LottieComponent } from 'ngx-lottie';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LottieComponent],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'recipe-sharing';
-  options = {
-    path: 'assets/a.json', // Point this to your Lottie animation JSON
-  };
 
 }
