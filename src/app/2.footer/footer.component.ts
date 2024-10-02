cbc0a547ee15930d918787a10143091aa316f8e3
jawad <jawad@.com>
2024-10-02 22:16:59 +0300

    add footer

--- src/app/2.footer/footer.component.ts
+++ src/app/2.footer/footer.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive, RouterModule } from '@angular/router';
+  import { LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
-  templateUrl: './footer.component.html',
+  imports: [LottieComponent, RouterModule, RouterLinkActive],
+templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  logo = {
+    path: 'assets/logo.json',
+  };
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { LottieComponent } from 'ngx-lottie';
 import { AboutComponent } from "./9.about/about.component";
 import { NavbarComponent } from "./1.navbar/navbar.component";
+import { FooterComponent } from "./2.footer/footer.component";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent, NavbarComponent],
+  imports: [RouterOutlet, AboutComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
