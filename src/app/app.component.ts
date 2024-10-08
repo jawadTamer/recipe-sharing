949f3249f2182c95e32496f0c83b28a342dfea83
Tarek-goda <tarekgoda92@gmail.com>
2024-10-09 02:40:26 +0300

    user-details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,13 +6,15 @@ import { AboutComponent } from "./9.about/about.component";
 import { NavbarComponent } from "./1.navbar/navbar.component";
 import { FooterComponent } from "./2.footer/footer.component";
 import { NgClass } from '@angular/common';
+import { UserDetailsComponent } from './10.user-details/user-details.component';
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AboutComponent, NavbarComponent, FooterComponent,NgClass],
+  imports: [RouterOutlet, AboutComponent, NavbarComponent, FooterComponent,NgClass,UserDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
