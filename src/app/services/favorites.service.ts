d2f24069a7499f8a1b89778911dd1316cd5162ed
Saja Malek <66196100+sajamalek90@users.noreply.github.com>
2024-10-17 03:13:04 +0300

    Update favorites.service.ts

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { RecipeInterface } from '../recipe-interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FavoritesService {
+  private favoriteRecipes: RecipeInterface[] = [];
+  private favoritesSubject = new BehaviorSubject<RecipeInterface[]>(this.favoriteRecipes);
 
-  constructor() { }
+  constructor() {}
+
+  getFavorites() {
+    return this.favoritesSubject.asObservable();
+  }
+
+  addToFavorites(recipe: RecipeInterface) {
+    if (!this.favoriteRecipes.find(r => r.id === recipe.id)) {
+      this.favoriteRecipes.push(recipe);
+      this.favoritesSubject.next([...this.favoriteRecipes]);
+    }
+  }
+
+  removeFromFavorites(recipeId: number) {
+    this.favoriteRecipes = this.favoriteRecipes.filter(r => r.id !== recipeId);
+    this.favoritesSubject.next([...this.favoriteRecipes]);
+  }
+
+  isFavorite(recipeId: number): boolean {
+    return this.favoriteRecipes.some(r => r.id === recipeId);
+  }
 }
