b22762f3c3821f95e12d155e7f32132ebe906c9c
Saja Malek <66196100+sajamalek90@users.noreply.github.com>
2024-10-18 15:58:53 +0300

    Update auth.guard.ts

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,4 +1,23 @@
 import { CanActivateFn, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
+
+  canActivate(): boolean {
+    const user = JSON.parse(localStorage.getItem('user') || 'null');
+    if (user && user.username) {
+      return true; // User is logged in
+    }
+    this.router.navigate(['/signup']); // Redirect to signup if not logged in
+    return false;
+  }
+}
 
 
 const isAuthenticated = (): boolean => {
@@ -8,11 +27,11 @@ const isAuthenticated = (): boolean => {
 
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const router = new Router(); 
+  const router = new Router();
   if (!isAuthenticated()) {
- 
+
     router.navigate(['signup']);
-    return false; 
+    return false;
   }
-  return true; 
+  return true;
 };
