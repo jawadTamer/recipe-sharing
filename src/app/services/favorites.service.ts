cea0bd1e36d7104255c47533ebd29cf14e35a606
jawad <jawad@.com>
2024-10-18 18:59:50 +0300

    .

--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -6,28 +6,6 @@ import { RecipeInterface } from '../recipe-interface';
   providedIn: 'root'
 })
 export class FavoritesService {
-  private favoriteRecipes: RecipeInterface[] = [];
-  private favoritesSubject = new BehaviorSubject<RecipeInterface[]>(this.favoriteRecipes);
+ 
 
-  constructor() {}
-
-  getFavorites() {
-    return this.favoritesSubject.asObservable();
-  }
-
-  addToFavorites(recipe: RecipeInterface) {
-    if (!this.favoriteRecipes.find(r => r.id === recipe.id)) {
-      this.favoriteRecipes.push(recipe);
-      this.favoritesSubject.next([...this.favoriteRecipes]);
-    }
-  }
-
-  removeFromFavorites(recipeId: number) {
-    this.favoriteRecipes = this.favoriteRecipes.filter(r => r.id !== recipeId);
-    this.favoritesSubject.next([...this.favoriteRecipes]);
-  }
-
-  isFavorite(recipeId: number): boolean {
-    return this.favoriteRecipes.some(r => r.id === recipeId);
-  }
 }
